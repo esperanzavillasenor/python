6b6339e65cd8bb36dfb139584a184297ef9a1f0c
esperanzavillasenor <42874137+esperanzavillasenor@users.noreply.github.com>
2018-10-03 10:06:51 -0700

    Add files via upload

---
+++ Ops1.py
@@ -0,0 +1,17 @@
+#Ops1.py
+"""
++	Addition 	x + y 	
+- 	Subtraction 	x - y 	
+* 	Multiplication 	x * y 	
+/ 	Division 	x / y 	
+% 	Modulus 	x % y 	
+** 	Exponentiation 	x ** y 	 
+// 	Floor division 	x // y 	 
+"""
+print ("33 + 17", 33 + 17)
+print ("33 - 17", 33 - 17)
+print ("33 * 17", 33 * 17)
+print ("33 / 17", 33 / 17)
+print ("33 % 17", 33 % 17)
+print ("33 ** 17", 33 ** 17)
+print ("37 // 17", 37 // 17)
